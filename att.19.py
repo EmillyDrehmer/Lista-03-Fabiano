a910d0b3ce676b93ce7e824eef59aa8b8475127b
EmillyDrehmer <emillydrehmer043@gmail.com>
2026-04-16 16:03:49 -0300

    Atividade 19 – Eficiência da Produção
    Você precisa avaliar o desempenho da produção.
     Implemente:
    • Cálculo de eficiência (%)
    • Comparação com meta

---
+++ att.19.py
@@ -0,0 +1,11 @@
+producao = 900
+capacidade = 1000
+
+eficiencia = (producao / capacidade) * 100
+
+print("Eficiência:", eficiencia, "%")
+
+if eficiencia >= 90:
+    print("Boa eficiência!")
+else:
+    print("Baixa eficiência!")
